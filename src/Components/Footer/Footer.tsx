9badff36d45adedb2f7dde266f490264c0d1a215
KoJem9Ka <bis21080000@gmail.com>
2022-05-01 14:50:03 +0300

    fixes

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,7 +4,17 @@ import Container from '../../HOC/Container/Container'
 
 const Footer: React.FC = () => (
   <Container gap='1rem' innerClasses={styles.footerInner} outerTag='footer'>
-    <p>KoJem9Ka Recipes on React</p>
+    <p>
+      KoJem9Ka Recipes on React:&nbsp;
+      <a
+        href='https://github.com/KoJem9Ka/react-recipes'
+        rel='noreferrer'
+        style={{ color: 'aqua' }}
+        target='_blank'
+      >
+        Repository
+      </a>
+    </p>
     <p>April 2022</p>
   </Container>
 )
--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <AppContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename='/react-recipes'>
         <App/>
       </BrowserRouter>
     </AppContextProvider>
